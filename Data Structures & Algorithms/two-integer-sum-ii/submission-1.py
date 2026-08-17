b73b503e554de5e6525b855fdcb75dd1923465ed
Isaac Jarrells <130888338+Nemmz@users.noreply.github.com>
2026-08-17 09:10:49 -0700

    Add: two-integer-sum-ii - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        front, end = 0, len(numbers) - 1
+        while front < end: #ensures that we won't pass the other pointer if the situation goes that long
+            if (numbers[front] + numbers[end] == target) is True: 
+                return [front+1, end+1] # returns the indexes if the target is meet, note the (+1) is to make it make sense to an non programmer that doesn't know it starts counting from zero.
+            elif numbers[front] + numbers[end] < target: # advances the front pointer if the number is bigger than the target
+                front += 1 # Note this only works on decreasing list otherwise would fail.
+            else:
+                end -= 1
+        
